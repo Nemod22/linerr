d0171045cc5f1ef7c773df89d00f5a11ca83bb82
Nemod22 <domen.zgalin@gmail.com>
2022-04-25 19:08:15 +0200

    1

---
+++ linreg.py
@@ -0,0 +1,58 @@
+import numpy as np
+
+def lsqfity(X, Y):
+    """
+    Calculate a "MODEL-1" least squares fit.
+
+    The line is fit by MINIMIZING the residuals in Y only.
+
+    The equation of the line is:     Y = my * X + by.
+
+    Equations are from Bevington & Robinson (1992)
+    Data Reduction and Error Analysis for the Physical Sciences, 2nd Ed."
+    pp: 104, 108-109, 199.
+
+    Data are input and output as follows:
+
+    my, by, ry, smy, sby = lsqfity(X,Y)
+    X     =    x data (vector)
+    Y     =    y data (vector)
+    my    =    slope
+    by    =    y-intercept
+    ry    =    correlation coefficient
+    smy   =    standard deviation of the slope
+    sby   =    standard deviation of the y-intercept
+
+    """
+
+    X, Y = map(np.asanyarray, (X, Y))
+
+    # Determine the size of the vector.
+    n = len(X)
+
+    # Calculate the sums.
+
+    Sx = np.sum(X)
+    Sy = np.sum(Y)
+    Sx2 = np.sum(X ** 2)
+    Sxy = np.sum(X * Y)
+    Sy2 = np.sum(Y ** 2)
+
+    # Calculate re-used expressions.
+    num = n * Sxy - Sx * Sy
+    den = n * Sx2 - Sx ** 2
+
+    # Calculate my, by, ry, s2, smy and sby.
+    my = num / den
+    by = (Sx2 * Sy - Sx * Sxy) / den
+    ry = num / (np.sqrt(den) * np.sqrt(n * Sy2 - Sy ** 2))
+
+    diff = Y - by - my * X
+
+    s2 = np.sum(diff * diff) / (n - 2)
+    smy = np.sqrt(n * s2 / den)
+    sby = np.sqrt(Sx2 * s2 / den)
+
+    return my, by, ry, smy, sby    
+
+print(lsqfity([0,2,4,6,8],[0,3,6,9,12]))
